d95addc9324a4249434db509bb63d54eb076f2fc
Nagaruu <bachduongpmd@gmail.com>
2021-01-27 15:07:19 +0700

    abcd

---
+++ Magento_Theme/requirejs-config.js
@@ -0,0 +1,11 @@
+var config = {
+    paths: {            
+            owlcarousel: "Magento_Theme/js/owl.carousel",
+            test : "Magento_Theme/js/test"
+        },   
+    shim: {
+        'owlcarousel': {
+            deps: ['jquery']
+        }
+    }
+};
